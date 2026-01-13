618ef6381362c373f7332c185739c77b3a886d65
spavila <spavila@liderit.es>
2026-01-13 16:08:47 +0100

    ej5

--- proyectos/PR1/src/ej4.py
+++ proyectos/PR1/src/ej4.py
@@ -2,4 +2,5 @@
 def maximo(lista: list[int]) -> int:
     # Devuelve el número máximo de la lista
     return max(lista)
+#max devuelve el número más alto de la lista
 print(maximo([3, 5, 2, 8, 1]))
---
+++ proyectos/PR1/src/ej5.py
@@ -0,0 +1,3 @@
+def invertir(texto: str) -> str:
+    # Devuelve el texto invertido
+    return texto[::-1]
